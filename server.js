26dd91617e00b33a17970d3207e6e99a19e72acb
THXBurke <rachelwheels@gmail.com>
2016-02-24 20:04:30 -0800

    debug

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require ('express');
-var server = express;
 var port = process.env.PORT || 8000;
-app = express();
+var app = express();
 app.use(express.static(__dirname + '/public'));
