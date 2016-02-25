8c2a38add0be6d9e2e1720f9abed8b5b5deba8f9
THXBurke <rachelwheels@gmail.com>
2016-02-24 20:12:16 -0800

    listen

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ var express = require ('express');
 var port = process.env.PORT || 8000;
 var app = express();
 app.use(express.static(__dirname));
+
+app.listen(port, function() {
+  console.log('Server listening on port ' + (port || 8000));
+});
